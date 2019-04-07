87eec17b7f1552635121105130ef936649290b96
yulheekim <yulkim2019@u.northwestern.edu>
2019-04-07 18:43:34 -0500

    gets a document

--- my-app/src/pages/Routine/Routine.js
+++ my-app/src/pages/Routine/Routine.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import firebase from '../../firebaseTest.js';
+
 
 import {
     AppBar,
@@ -11,6 +13,24 @@ import './styles.css';
 // import AppBar from '../../components/Heading/AppBar.js';
 
 class RoutineComponent extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      routines: {},
+      key: ''
+    };
+  }
+    componentDidMount() {
+        const ref = firebase.firestore().collection('routines').doc("1");
+        ref.get().then((doc) => {
+            if (doc.exists) {
+                console.log(doc.data());
+                this.setState({routines: doc.data(), key: doc.id, isLoading: false});
+            } else {
+                console.log("No such document!");
+            }
+        });
+    }
     render() {
         return (
             <div>
