8b79f17f71d5c9075e9dd22cffd0bbbe6f4d2d38
Xin Tong <xintongbupt@outlook.com>
2019-04-07 19:23:29 -0500

    Made edit to Button

--- my-app/src/components/AppBar/AppBar.js
+++ my-app/src/components/AppBar/AppBar.js
@@ -8,6 +8,12 @@ import Typography from '@material-ui/core/Typography';
 const styles = {
   root: {
     flexGrow: 1,
+    color:'#3f51b5',
+    textalign:'center',
+    fontStyle:'italic',
+    fontWeight:"bold",
+    fontSize:'30px',
+    // border:'1px #666 solid',
   },
 };
 
@@ -16,7 +22,7 @@ function SimpleAppBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="#000000">
+      <AppBar position="static" color="#000000" style={styles}>
         <Toolbar>
           <Typography variant="h6" color="inherit">
             WorkIt
