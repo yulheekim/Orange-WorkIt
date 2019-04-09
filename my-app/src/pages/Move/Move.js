6493ecbcdb2aa65e48f18c9fa981acd0177519ec
Keren Park <keren@dhcp-10-105-115-206.wireless.northwestern.private>
2019-04-08 20:00:20 -0500

    make timer font larger and add a card around it

--- my-app/src/pages/Move/Move.js
+++ my-app/src/pages/Move/Move.js
@@ -1,35 +1,82 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Timer from 'react-compound-timer';
+import Card from '@material-ui/core/Card';
 
 import {
     AppBar
 } from '../../components';
 import './styles.css';
 
-class MoveComponent extends Component {
-    render() {
-        return (
-            <div>
-                <AppBar />
-                <h2> [move name] </h2>
-                <Timer
-                    initialTime={45000} // hardcode. replace.
-                    direction="backward"
-                >
-                    {() => ( // the formatValue attribute formats the seconds such that the leading 0 is displayed on single digits
-                        <React.Fragment>
-                            <Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}:`}/>
-                            <Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}/> 
-                        </React.Fragment>
-                    )}
-                </Timer>
+const styles = theme => ({
+    card: {
+      width: '100%',
+      justify: "center",
+      font: '100px',
+    },
+  });
+
+function MoveComponent(props) {
+    const { classes } = props;
+    return (
+        <div>
+            <AppBar /> 
+            <div class="page-content">
+                <h2> Plank </h2>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/aCa8R9II8F0?controls=0&start=54&end=83" 
                 frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <br />
+                <br />
+                <Card>
+                    <div class="timer">
+                    <Timer
+                            initialTime={45000} // hardcode. replace.
+                            direction="backward"
+                        >
+                            {() => ( // the formatValue attribute formats the seconds such that the leading 0 is displayed on single digits
+                                <React.Fragment>
+                                    <Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}:`}/>
+                                    <Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}/> 
+                                </React.Fragment>
+                            )}
+                        </Timer>
+                    </div>
+                </Card> 
             </div>
-        );
-    }
-}
+        </div>
+    )
+};
+
+// class MoveComponent extends Component {
+//     render() {
+//         return ( // hardcode <h2>. replace.
+//             <div>
+//                 <AppBar /> 
+//                 <div class="page-content">
+//                 <h2> Plank </h2>
+//                     <Timer
+//                         initialTime={45000} // hardcode. replace.
+//                         direction="backward"
+//                     >
+//                         {() => ( // the formatValue attribute formats the seconds such that the leading 0 is displayed on single digits
+//                             <React.Fragment>
+//                                 <Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}:`}/>
+//                                 <Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}/> 
+//                             </React.Fragment>
+//                         )}
+//                     </Timer>
+//                 <iframe width="560" height="315" src="https://www.youtube.com/embed/aCa8R9II8F0?controls=0&start=54&end=83" 
+//                 frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+MoveComponent.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
 
 export { MoveComponent };
 
