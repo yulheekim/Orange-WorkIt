74040a779b8a73fbbafa717ca126b855dba539f6
Xin Tong <xintongbupt@outlook.com>
2019-04-14 11:46:09 -0500

    edit

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -2,3 +2,4 @@ export {default as AppBar} from './AppBar/AppBar';
 export {default as Paper} from './Paper/Paper';
 export {default as List} from './List/List';
 export {default as Button} from './Button/Button';
+export {default as TextField} from './TextField/TextField';
