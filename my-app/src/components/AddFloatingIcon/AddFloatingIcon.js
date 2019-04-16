9f36888cfd61489cb5f1e5e7d7a8e8d696eefde6
Keren Park <parkhk10@gmail.com>
2019-04-16 12:09:03 -0500

    link floating buttons to add move and routine page

--- my-app/src/components/AddFloatingIcon/AddFloatingIcon.js
+++ my-app/src/components/AddFloatingIcon/AddFloatingIcon.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import { Link } from 'react-router-dom';
 
 const styles = theme => ({
   fab: {
@@ -20,7 +21,7 @@ function FloatingActionButtons(props) {
     const { classes } = props;
     return (
       <div>
-        <Fab color="primary" variant="extended" aria-label="Delete" className={classes.fab}>
+        <Fab color="primary" variant="extended" aria-label="Delete" className={classes.fab} component={Link} to={props.link}>
         <AddIcon className={classes.extendedIcon} />
           {props.name}
         </Fab>
