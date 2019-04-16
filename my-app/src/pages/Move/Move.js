26d1d018ddaa45cafc41ca78a92692b47f88a077
Keren Park <parkhk10@gmail.com>
2019-04-16 12:03:27 -0500

    clean move code and styling

--- my-app/src/pages/Move/Move.js
+++ my-app/src/pages/Move/Move.js
@@ -25,89 +25,6 @@ const styles = theme => ({
     },
   });
 
-//   function loopMoves(moves) {
-//     const img_lst = [squats, plank, crunches];
-//     return _.map(moves, (move, index) => {
-//         return (
-//           <ListItem button key={index}>
-//               <Avatar src={img_lst[index]}>
-//               </Avatar>
-//               <ListItemText primary={move.name} secondary={"time: " + move.total_time + "sec"} />
-//               <KeyboardArrowRight/>
-//           </ListItem>
-//             )
-//     });
-// }
-
-// function loadNext(move_index) {
-//     increment_move_index(move_index);
-//
-//
-// };
-//
-// const MoveComponent = (props, { moves, move_index }) => {
-//     console.log(props.location.state);
-//     setTimeout(() => console.log(move_index), 5000);
-//     return (
-//         <div>
-//             <AppBar />
-//             <br />
-//             <br />
-//             <div class="page-content">
-//             <div class="resp-container">
-//             <iframe class="resp-iframe" width="560" height="315" src="https://www.youtube.com/embed/ynUw0YsrmSg?version=3&controls=0&start=54&end=83&autoplay=1"
-//                 frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-//             </div>
-//                 <br />
-//                 <h2>{moves[move_index].name}</h2>
-//                 <Card>
-//                     <div class="timer">
-//                     <Timer
-//                             initialTime={1000} // hardcode. replace.
-//                             direction="backward"
-//                             checkpoints={[
-//                                 {time: 0,
-//                                 callback: () => loadNext(move_index) } // callback function for when timer reaches 0
-//                             ]}
-//                         >
-//                             {( { pause, resume } ) => ( // the formatValue attribute formats the seconds such that the leading 0 is displayed on single digits
-//                                 <React.Fragment>
-//                                 <div>
-//                                     <Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}:`}/>
-//                                     <Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`}/>
-//                                 </div>
-//                                 <div>
-//                                     <Button variant="contained" color="primary" onClick={pause}>Pause</Button>  <Button variant="contained" color="primary" onClick={resume}>Resume</Button>
-//                                 </div>
-//                                 </React.Fragment>
-//                             )}
-//                         </Timer>
-//                     </div>
-//                 </Card>
-//             </div>
-//         </div>
-//     )
-// };
-//
-// export { MoveComponent };
-//
-// const mapStateToProps = (state, ownProps) => {
-//     const { reducer } = state;
-//     const { loading, moves, move_index } = reducer;
-//     return {
-//         ...ownProps,
-//         loading,
-//         moves,
-//         move_index
-//     };
-// };
-//
-// export const Move = connect(mapStateToProps, {
-//     load_moves,
-//     increment_move_index
-// })(MoveComponent);
-
-
 //====================
 // updating move_index using redux
 //====================
@@ -123,10 +40,6 @@ class MoveComponent extends Component {
         };
 
     };
-    // componentDidMount() {
-    //     //console.log(this.props.location.state.move_time)
-    //     this.state.index = 0;
-    // }
 
     flipToNext(move_index) {
         if (this.props.move_index >= this.props.moves.length - 1) {
