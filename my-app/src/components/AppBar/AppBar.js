f6a79b0b313c284a2277b8406464b87f0c2e8ed9
Jayden Soni <jrs@u.northwestern.edu>
2019-04-17 13:44:23 -0500

    Home button added

--- my-app/src/components/AppBar/AppBar.js
+++ my-app/src/components/AppBar/AppBar.js
@@ -4,6 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import { Link } from 'react-router-dom'
 
 const styles = {
   root: {
@@ -23,8 +26,11 @@ function SimpleAppBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" style={styles}>
+      <AppBar position="static">
         <Toolbar>
+        <IconButton aria-label="Home" color="inherit" component={Link} to="/routines">
+          <HomeIcon/>
+        </IconButton>
           <Typography variant="h6" color="inherit">
             WorkIt
           </Typography>
